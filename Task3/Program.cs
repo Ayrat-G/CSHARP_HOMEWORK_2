0e7bd974c68f075e1f51968287a6e5f29c4ddaa7
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-26 00:06:41 +0500

    Программа, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Console.WriteLine("Введите натуральное число из отрезка [10; 99]: "); 
+int num = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = num / 10;
+int digit2 = num % 10;
+
+if (digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+}
+else
+{
+    Console.WriteLine(digit2);
+}
