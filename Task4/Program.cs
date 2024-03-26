af9b8a2916f0310f818c7b8fb097745e08c23118
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-26 23:53:50 +0500

    Напиcfk программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую
+
+Console.WriteLine("Введите натуральное число N: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i = i+1;
+}
